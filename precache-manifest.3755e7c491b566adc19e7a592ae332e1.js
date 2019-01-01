043c8ed495173b012d68d2421effdd87e07654b2
Aleck Landgraf <aleck.landgraf@airbnb.com>
2019-01-01 15:02:51 -0800

    Updates

---
+++ precache-manifest.3755e7c491b566adc19e7a592ae332e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7881a4e1ecc1bd3f1e8",
+    "url": "/MTG-recognizer/static/js/runtime~main.a7881a4e.js"
+  },
+  {
+    "revision": "a2c82f8544f7d235d315",
+    "url": "/MTG-recognizer/static/js/main.a2c82f85.chunk.js"
+  },
+  {
+    "revision": "7ab140479779e9f0ea42",
+    "url": "/MTG-recognizer/static/js/1.7ab14047.chunk.js"
+  },
+  {
+    "revision": "a2c82f8544f7d235d315",
+    "url": "/MTG-recognizer/static/css/main.390d6a49.chunk.css"
+  },
+  {
+    "revision": "2763fdb5329a9c810f284733c9c499df",
+    "url": "/MTG-recognizer/index.html"
+  }
+];
