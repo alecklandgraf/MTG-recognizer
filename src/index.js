11d143307055341a8fa9e02e159492a0973c2ee4
Aleck Landgraf <aleck.landgraf@airbnb.com>
2019-01-02 10:38:53 -0800

    some hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const App = React.lazy(() => import("./App"));
+
+if (SpeechRecognition) {
+  ReactDOM.render(
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>,
+    document.getElementById("root")
+  );
+} else {
+  ReactDOM.render(
+    <div className="App-header">
+      Speech recognition unsupported on this browser, try chrome desktop,
+      android chrome, or Firefox
+    </div>,
+    document.getElementById("root")
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
